da5a57a81dc0cc85e05182f51f7e7a35b182b4e8
Ellen <ellen.deveth@hotmail.com>
2022-04-28 12:07:40 +0200

    feat: add login, signup and dashboard components to vue

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+  <div>
+        <h1>Login</h1>
+        <input type="text" placeholder="Username" v-model="username">
+        <input type="text" placeholder="Email" v-model="message">
+        <input type="password" placeholder="Password">
+        <input type="submit" @click="addMessage" value="Login">
+    </div>
+</template>
+
+<style scoped>
+
+</style>
