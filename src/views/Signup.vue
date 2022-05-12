73f2132a1ecc75a23159eb56b422c39f17d75d81
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-12 12:19:21 +0200

    feat: fetch signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,6 +1,28 @@
 <script setup>
 import { ref } from 'vue'
+let firstname = ref('');
+let lastname = ref('');
+let email = ref('');
+let password = ref('');
 
+function Signup(e) {
+e.preventDefault();
+    fetch("http://localhost:3001/users/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            firstname: firstname.value,
+            lastname: lastname.value,
+            email: email.value,
+            password: password.value,
+        })
+    }).then(res => res.json())
+    .then(json => {
+        console.log(json);
+    })
+}
 
 </script>
 
@@ -12,8 +34,8 @@ import { ref } from 'vue'
             <input type="text" placeholder="Firstname" v-model="firstname" class="inputField">
             <input type="text" placeholder="Lastname" v-model="lastname" class="inputField">
             <input type="text" placeholder="Email" v-model="email" class="inputField">
-            <input type="password" placeholder="Password" class="inputField">
-            <input type="submit" @click="addMessage" value="Submit" class="btn">
+            <input type="password" placeholder="Password" v-model="password" class="inputField">
+            <input type="submit" @click="Signup" value="Submit" class="btn">
         </form>
 
         <router-link to="/" class="link">Login here</router-link>
