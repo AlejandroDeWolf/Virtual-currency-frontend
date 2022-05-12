0ddefe9a169cb22c7569b330b246e29aa8cd9f6e
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-05-12 11:11:49 +0200

    feat: folder with views (pages)

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+    <div class="card">
+        <h1>Login</h1>
+
+        <form action="">
+            <input type="text" placeholder="Email" v-model="email" class="inputField">
+            <input type="password" placeholder="Password" class="inputField">
+            <input type="submit" @click="addMessage" value="Submit" class="btn">
+        </form>
+
+        <router-link to="/signup" class="link">Sign up here</router-link>
+    </div>
+</template>
---
+++ src/views/Signup.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+    <div class="card">
+        <h1>Sign up</h1>
+
+        <form action="">
+            <input type="text" placeholder="Firstname" v-model="firstname" class="inputField">
+            <input type="text" placeholder="Lastname" v-model="lastname" class="inputField">
+            <input type="text" placeholder="Email" v-model="email" class="inputField">
+            <input type="password" placeholder="Password" class="inputField">
+            <input type="submit" @click="addMessage" value="Submit" class="btn">
+        </form>
+
+        <router-link to="/" class="link">Login here</router-link>
+    </div>
+</template>
+
