3b0b26881941c004c6aa090e6f33a5aaabf1c153
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-20 19:57:49 +0200

    feat: add transaction view to routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
 import Signup from '../views/Signup.vue';
 import Dashboard from '../views/Dashboard.vue';
+import Send from '../views/Send.vue';
 
 const routes = [
     { path: '/', name: 'Login', component: Login },
     { path: '/signup', name: 'Signup', component: Signup },
     { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+    { path: '/send', name: 'Send', component: Send },
 ]
 
 const router = createRouter({
