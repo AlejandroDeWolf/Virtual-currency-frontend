07cd96c303e027b19ecc43de6660c83cdc0d7e6f
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-20 21:53:53 +0200

    feat: add leaderboard to routers in vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Login from '../views/Login.vue';
 import Signup from '../views/Signup.vue';
 import Dashboard from '../views/Dashboard.vue';
 import Send from '../views/Send.vue';
+import Leaderboard from '../views/Leaderboard.vue';
 
 const routes = [
     { path: '/', name: 'Login', component: Login },
     { path: '/signup', name: 'Signup', component: Signup },
     { path: '/dashboard', name: 'Dashboard', component: Dashboard },
     { path: '/send', name: 'Send', component: Send },
+    { path: '/leaderboard', name: "Leaderboard", component: Leaderboard}
 ]
 
 const router = createRouter({
