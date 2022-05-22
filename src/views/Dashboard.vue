c3adad1d3d6bef8c1188d5ec94811a6b630ddbac
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-22 14:23:24 +0200

    feat: remove transactions from dashboard and add button to navigate to transactions page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -31,29 +31,6 @@ function getUser() {
         })
 }
 
-
-function getTransactions(){
-    let token = localStorage.getItem("token");
-
-    fetch(config.url + "/transactions", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": token
-        },
-    }).then(res => res.json())
-        .then(json => {
-            if(json.status === "success"){
-                transactions.value = json.data.transactions;
-                sender.value = json.data.user;
-            }
-            console.log(json);
-
-        }).catch(err => {
-            console.log(err)
-        })
-}
-
 //get transaction details from one by id
 function Details() {
     let token = localStorage.getItem("token");
@@ -74,53 +51,24 @@ function Details() {
 
 onMounted(() => {
     getUser();
-    getTransactions();
 })
 </script>
 
 <template>
     <div class="card">
         <h3 class="user">{{ firstname + " " + lastname }}</h3>
+
         <div class="balance">
             <h2 class="balance__amount"> {{ amount }}</h2>
             <p class="balance__text">credits</p>
         </div>
-       <div>
-            <router-link to="/send" class="btn btn--dashboard">Credits verzenden</router-link>
-        </div>
-        <div class="transactions">
-            <ul class="transactions__list">
-                <li class="transactions__list__item" v-bind:key="index" v-for="t, index in transactions">
-                    <!-- <div @click="Details">
-                        From: {{ t.receiver }}
-                    </div>   -->
-                    <div  v-if="t.receiver === firstname" v-bind:key="index" v-for="s, index in sender">
-                        <div class="item__person" v-if="s._id === t.sender">
-                            Van:  {{ s.firstname }}
-                        </div>
-                    </div> 
-                    <div class="item__person" v-else>
-                       Aan: {{ t.receiver}}
-                    </div> 
 
-                    <div class="item__count" v-if="t.receiver === firstname">
-                        <div>
-                            + {{t.amount}} coins
-                        </div>
-                        <div>
-                            {{ t.reason}}
-                        </div>
-                    </div>
-                    <div class="item__count" v-else>
-                        <div>
-                            - {{t.amount}} coins
-                        </div>
-                        <div>
-                            {{ t.reason}}
-                        </div>
-                    </div>
-                </li>
-            </ul>
+        <div class="btn btn--dashboard">
+            <router-link to="/send">Credits verzenden</router-link>
+        </div>
+
+        <div class="btn btn--dashboard">
+            <router-link to="/transactions">Bekijk transacties</router-link>
         </div>
     </div>
 </template>
