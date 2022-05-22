098dbbde0587b573971d3c5afdc230280718fc0e
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-22 16:45:56 +0200

    feat: create components directory with navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+
+<template>
+    <div class="nav">
+        <div class="link">
+            <router-link to="/dashboard" class="nav__link nav__link--nav">
+                <div class="icon"><img src="../assets/home-inactive.svg" alt=""></div>
+                <div>Home</div>
+            </router-link>
+        </div>
+        <div class="link">
+            <router-link to="/transactions" class="nav__link nav__link--nav">
+                <div class="icon"><img src="../assets/transactions-inactive.svg" alt=""></div>
+                <div>Transacties</div>
+            </router-link>
+        </div>
+        <div class="link">
+            <router-link to="/send" class="nav__link nav__link--nav">
+                <div class="icon"><img src="../assets/send-inactive.svg" alt=""></div>
+                <div>Sturen</div>
+            </router-link>
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+</style>
