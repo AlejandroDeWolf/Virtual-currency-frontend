1b7fd3a27262f31f0dc23f6d6fbf0437723bb589
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-22 23:31:49 +0200

    fix: remove classnames for duplicates

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,19 +5,19 @@
 
 <template>
     <div class="nav">
-        <div class="link">
+        <div>
             <router-link to="/dashboard" class="nav__link nav__link--nav">
                 <div class="icon"><img src="../assets/home-inactive.svg" alt=""></div>
                 <div>Home</div>
             </router-link>
         </div>
-        <div class="link">
+        <div>
             <router-link to="/transactions" class="nav__link nav__link--nav">
                 <div class="icon"><img src="../assets/transactions-inactive.svg" alt=""></div>
                 <div>Transacties</div>
             </router-link>
         </div>
-        <div class="link">
+        <div >
             <router-link to="/send" class="nav__link nav__link--nav">
                 <div class="icon"><img src="../assets/send-inactive.svg" alt=""></div>
                 <div>Sturen</div>
