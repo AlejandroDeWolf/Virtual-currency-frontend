934ace2b0d197fe670d0c81f6b964b71dc2c61f9
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-22 14:20:32 +0200

    feat: routing for transactions page

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,15 @@ import Signup from '../views/Signup.vue';
 import Dashboard from '../views/Dashboard.vue';
 import Send from '../views/Send.vue';
 import Leaderboard from '../views/Leaderboard.vue';
+import Transactions from '../views/Transactions.vue';
 
 const routes = [
     { path: '/', name: 'Login', component: Login },
     { path: '/signup', name: 'Signup', component: Signup },
     { path: '/dashboard', name: 'Dashboard', component: Dashboard },
     { path: '/send', name: 'Send', component: Send },
-    { path: '/leaderboard', name: "Leaderboard", component: Leaderboard}
+    { path: '/leaderboard', name: "Leaderboard", component: Leaderboard },
+    { path: '/transactions', name: "Transactions", component: Transactions },
 ]
 
 const router = createRouter({
