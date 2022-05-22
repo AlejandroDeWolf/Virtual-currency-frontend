efb7da2b318f0df1be896df3d73c3582734a1725
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-22 14:16:11 +0200

    feat: seperate page to view transactions

---
+++ src/views/Transactions.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { config } from '../../config/config'
+
+let username = ref('');
+let transactions = ref('');
+let sender = ref('');
+
+
+function getTransactions() {
+    let token = localStorage.getItem("token");
+
+    fetch(config.url + "/transactions", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": token
+        },
+    }).then(res => res.json())
+        .then(json => {
+            if (json.status === "success") {
+                transactions.value = json.data.transactions;
+                sender.value = json.data.user;
+            }
+            console.log(json);
+
+        }).catch(err => {
+            console.log(err)
+        })
+}
+
+//get transaction details from one by id
+function Details() {
+    let token = localStorage.getItem("token");
+    let id = transactions.value[0]._id;
+    //console.log(transactions.value[0]._id)
+    fetch(config.url + "/transactions/" + id, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": token
+        }
+    }).then(res => res.json())
+        .then(json => {
+            console.log(json);
+
+        })
+}
+
+onMounted(() => {
+    getTransactions();
+})
+</script>
+
+<template>
+    <div class="card">
+        <h2>Mijn transacties</h2>
+
+        <div class="transactions">
+            <ul class="transactions__list">
+                <li class="transactions__list__item" v-bind:key="index" v-for="t, index in transactions">
+                    <div v-if="t.receiver === username" v-bind:key="index" v-for="s, index in sender">
+                        <div class="item__person" v-if="s._id === t.sender">
+                            Van: {{ s.username }}
+                        </div>
+                    </div>
+                    <div class="item__person" v-else>
+                        Aan: {{ t.receiver }}
+                    </div>
+
+                    <div class="item__count" v-if="t.receiver === username">
+                        <div>
+                            + {{ t.amount }} coins
+                        </div>
+                        <div>
+                            {{ t.reason }}
+                        </div>
+                    </div>
+                    <div class="item__count" v-else>
+                        <div>
+                            - {{ t.amount }} coins
+                        </div>
+                        <div>
+                            {{ t.reason }}
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
