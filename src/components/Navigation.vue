e3804040d32a27656c4951fee31d5af5d5cae7c6
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-27 12:48:03 +0200

    style: add class to navigation links

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,19 +5,19 @@
 
 <template>
     <div class="nav">
-        <div>
+        <div class="link">
             <router-link to="/dashboard" class="nav__link nav__link--nav">
                 <div class="icon"><img src="../assets/home-inactive.svg" alt=""></div>
                 <div>Home</div>
             </router-link>
         </div>
-        <div>
+        <div class="link">
             <router-link to="/transactions" class="nav__link nav__link--nav">
                 <div class="icon"><img src="../assets/transactions-inactive.svg" alt=""></div>
                 <div>Transacties</div>
             </router-link>
         </div>
-        <div >
+        <div class="link">
             <router-link to="/send" class="nav__link nav__link--nav">
                 <div class="icon"><img src="../assets/send-inactive.svg" alt=""></div>
                 <div>Sturen</div>
@@ -25,6 +25,8 @@
         </div>
 
     </div>
+
+    <img src="logo.png" alt="">
 
 </template>
 
